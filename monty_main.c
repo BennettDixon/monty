22e416a5ef51c5137318746746f2290a135b32c1
BennettDixon <bennettdixon16@gmail.com>
2018-12-19 00:21:08 +0000

    Freed file pointer, no leaks, but still memory errors

--- monty_main.c
+++ monty_main.c
@@ -16,11 +16,14 @@ char **op_toks = NULL;
 int main(int argc, char **argv)
 {
 	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
 		return (usage_error());
 	script_fd = fopen(argv[1], "r");
 	if (script_fd == NULL)
 		return (f_open_error(argv[1]));
-	return (run_monty(script_fd));
+	exit_code = run_monty(script_fd);
+	free(script_fd);
+	return (exit_code);
 }
