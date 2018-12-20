c58e489b225c12a45f2aa0774d0ab7af6ce25375
bdbaraban <375@holbertonschool.com>
2018-12-20 01:02:44 +0000

    Add documentation for monty_pint

--- monty_funcs_3.c
+++ monty_funcs_3.c
@@ -12,7 +12,11 @@ void monty_nop(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 }
 
-
+/**
+ * monty_pint - Prints the top value of a stack_t linked list.
+ * @stack: A pointer to the top of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
 void monty_pint(stack_t **stack, unsigned int line_number)
 {
 	if ((*stack)->next == NULL)
